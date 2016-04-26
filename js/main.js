f7e6c1937ceacf8ac40a88c6908f1601c2e2c6c8
David Factor <david.factor@industrieit.com>
2016-04-27 09:45:27 +1000

    new style changes

--- js/main.js
+++ js/main.js
@@ -21,22 +21,22 @@ $(document).ready(function() {
      });
   });
 
-  //  offset hash location when arriving from external link
-  //
-  // grab target from URL hash (i.e. www.mysite.com/page-a.html#target-name)
-  var target = window.location.hash;
+  // //  offset hash location when arriving from external link
+  // //
+  // // grab target from URL hash (i.e. www.mysite.com/page-a.html#target-name)
+  // var target = window.location.hash;
 
-  // only try to scroll to offset if target has been set in location hash
-  if ( target != '' ){
-    var $target = $(target);
+  // // only try to scroll to offset if target has been set in location hash
+  // if ( target != '' ){
+  //   var $target = $(target);
 
-    $root.stop().animate({
-      'scrollTop': $target.offset().top
-    }, 500,'swing', function () {
-      // when done, add hash to url
-      window.location.hash = target ;
-    });
-  };
+  //   $root.stop().animate({
+  //     'scrollTop': $target.offset().top
+  //   }, 500,'swing', function () {
+  //     // when done, add hash to url
+  //     window.location.hash = target ;
+  //   });
+  // };
 
 
 // get info ajax request
@@ -52,35 +52,41 @@ $(document).ready(function() {
       var data = JSON.parse(response);
       console.log(data);
       var memberCount = data.n_members;
-      document.getElementById("member-count").innerHTML = memberCount.toString();
-
-      // var utcSeconds = 1234567890;
-      // var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-      // d.setUTCSeconds(utcSeconds);
-      // console.log(d)
-
-      var currentSeconds = new Date().getTime() / 1000;
-      var lastMember = data.last_member_signup;
-      // var timeSince =  currentSeconds - lastMember
-      // console.log(timeSince);
-
-      function toTime(seconds) {
-        var d = new Date(0);
-        d.setUTCSeconds(seconds)
-        return d;
-      }
-
-var now = moment();
-console.log(lastMember);
-var a = moment.duration(lastMember, 'ms');
-var b = moment.duration(now, 'ms');
-var timeSince = a.subtract(b);
-// a.subtract(b).hours;
-console.log(timeSince)
-// console.log(moment.duration(timeSince, 's'));
+      document.getElementById("js-member-count").innerHTML = memberCount.toString();
 
 
-console.log(toTime(lastMember))
+
+
+
+
+
+// // var utcSeconds = 1234567890;
+//       // var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+//       // d.setUTCSeconds(utcSeconds);
+//       // console.log(d)
+
+//       var currentSeconds = new Date().getTime() / 1000;
+//       var lastMember = data.last_member_signup;
+//       // var timeSince =  currentSeconds - lastMember
+//       // console.log(timeSince);
+
+//       function toTime(seconds) {
+//         var d = new Date(0);
+//         d.setUTCSeconds(seconds)
+//         return d;
+//       }
+
+// var now = moment();
+// console.log(lastMember);
+// var a = moment.duration(lastMember, 'ms');
+// var b = moment.duration(now, 'ms');
+// var timeSince = a.subtract(b);
+// // a.subtract(b).hours;
+// console.log(timeSince)
+// // console.log(moment.duration(timeSince, 's'));
+
+
+// console.log(toTime(lastMember))
 
 
     },
