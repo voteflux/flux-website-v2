d5744f0e69d55f182dd880f00507d5d482638a58
David Factor <david.factor@industrieit.com>
2016-04-26 17:05:39 +1000

    ajax call working for get member count

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 // animate scroll behaviour on side menu
   $("a[href^='#']").on('click', function(e) {
-console.log("clickerd");
+
     // prevent default anchor click behavior
     e.preventDefault();
        // store hash
@@ -20,10 +20,6 @@ console.log("clickerd");
        window.location.hash = hash;
      });
   });
-
-
-
-
 
   //  offset hash location when arriving from external link
   //
@@ -42,4 +38,54 @@ console.log("clickerd");
     });
   };
 
+
+// get info ajax request
+  $.ajax({
+    url: "https://api.voteflux.org/getinfo",
+    data: {
+      format: 'json'
+    },
+    error: function() {
+      console.log('error');
+    },
+    success: function(response) {
+      var data = JSON.parse(response);
+      console.log(data);
+      var memberCount = data.n_members;
+      document.getElementById("member-count").innerHTML = memberCount.toString();
+
+      // var utcSeconds = 1234567890;
+      // var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+      // d.setUTCSeconds(utcSeconds);
+      // console.log(d)
+
+      var currentSeconds = new Date().getTime() / 1000;
+      var lastMember = data.last_member_signup;
+      // var timeSince =  currentSeconds - lastMember
+      // console.log(timeSince);
+
+      function toTime(seconds) {
+        var d = new Date(0);
+        d.setUTCSeconds(seconds)
+        return d;
+      }
+
+var now = moment();
+console.log(lastMember);
+var a = moment.duration(lastMember, 'ms');
+var b = moment.duration(now, 'ms');
+var timeSince = a.subtract(b);
+// a.subtract(b).hours;
+console.log(timeSince)
+// console.log(moment.duration(timeSince, 's'));
+
+
+console.log(toTime(lastMember))
+
+
+    },
+    type: 'GET'
+  })
 });
+
+
