c3474687ac0a4d1dc7057a52ed97cf44d21b2a5a
Max Kaye <m@xk.io>
2017-09-11 09:39:17 +1000

    Improve dashboards
    - Add ramda dependency (better than lodash)
    - add common.js from api
    - start refactoring out graph logic
    - implement donation graphs

---
+++ js/common.js
@@ -0,0 +1,83 @@
+// http://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
+function getParam(val) {
+  var result = undefined,
+    tmp = [];
+  location.search
+  //.replace ( "?", "" )
+  // this is better, there might be a question mark inside
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+  return result;
+}
+
+// modified above
+function hashParam(val) {
+  var result = undefined,
+    tmp = [];
+  location.hash
+  //.replace ( "#", "" )
+  // this is better, there might be a hash inside
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+  return result;
+}
+
+function getAuthToken() {
+  var getParamS = getParam('s');
+  var lsS = localStorage.getItem('s');
+  var hashS = hashParam('s');
+  if (getParamS) {
+    return getParamS;
+  } else if (hashS) {
+    return hashS;
+  }
+  return lsS;
+}
+
+if (getParam('s') !== undefined) {
+  localStorage.setItem('s', getParam('s'));
+}
+
+if (hashParam('s') !== undefined) {
+  localStorage.setItem('s', hashParam('s'));
+}
+
+
+function flux_api(path) {
+  if (path.slice(0, 3) !== 'api') {
+    path = 'api/v0/' + path;
+  }
+  if (!getParam('prod') && (getParam('debug') || window.location.hostname == 'localhost' /* || window.location.hostname == '127.0.0.1' */)) {
+    return "http://localhost:5000/" + path;
+  }
+  if (window.location.hostname == "flux-api-dev.herokuapp.com" && !getParam('prod')) {
+    return "http://flux-api-dev.herokuapp.com/" + path;
+  }
+  return "https://api.voteflux.org/" + path;
+};
+
+
+function handle_output_model_decorator(host_obj, field_name) {
+  var r = function (data) {
+    host_obj[field_name] = JSON.stringify(data);
+    console.log('Got some data!', data);
+  }
+  return r;
+}
+
+function jsonDumps(obj) {
+  return JSON.stringify(obj);
+}
+
+
+function tsToDate(ts) {
+  return new Date(1000 * ts);
+}
---
+++ js/dashboardGraphLogic.js
@@ -0,0 +1,112 @@
+const startOfFYGen = () => {
+  const now = new Date();
+  const month = now.getMonth();
+  if (month >= 6) {  // js months [0,11]; 6 => July; days are [1,31] // facepalm
+    return new Date(now.getFullYear(), 6, 1);
+  }
+  return new Date(now.getFullYear() - 1, 6, 1);
+}
+
+const startOfFY = startOfFYGen();
+console.log('Start of FY', startOfFY);
+
+
+const isThisFY = x => x >= startOfFY;
+
+
+const pushCoords = (d, data, filter = () => true) => {
+  if (filter(d.ts, d.amount)) {
+    data.x.push(d.ts);
+    data.y.push(d.amount);
+  }
+};
+
+
+const pushCoordsCumulative = (d, data, filter = () => true) => {
+  const last = R.tail(data.y) || 0.0;
+  pushCoords({...d, amount: last + d.amount}, data, filter);
+}
+
+
+const processDonationGraphs = function (data) {
+  const donationLog = R.pipe(
+    R.reverse,
+    R.map((obj) => ({...obj, ts: tsToDate(obj.ts), amount: parseFloat(obj.amount)}))
+  )(data.data.donations);
+
+  console.log('Donation Log: ', donationLog);
+
+
+  const donationDataTotal = {
+    x: [], y: [], type: 'scatter',
+    mode: 'markers', marker: {size: 10}
+  };
+  const donationDataCumulative = {x: [], y: [], type: 'scatter'};
+  const donationDataByJuriObj = {};
+  const donationDataCumulativeStateObj = {};
+
+
+  R.map(d => {
+    pushCoords(d, donationDataTotal, isThisFY);
+    if (donationDataByJuriObj[d.branch] === undefined) {
+      donationDataByJuriObj[d.branch] = R.clone({
+        x: [], y: [], type: 'scatter',
+        mode: 'markers', marker: {size: 10}
+      });
+    }
+    if (donationDataCumulativeStateObj[d.branch] === undefined) {
+      donationDataCumulativeStateObj[d.branch] = R.clone({x: [], y: [], type: 'scatter'});
+    }
+    pushCoords(d, donationDataByJuriObj[d.branch], isThisFY);
+    pushCoordsCumulative(d, donationDataCumulativeStateObj[d.branch])
+  }, donationLog);
+
+
+  let lastY = 0;
+  R.map(([x, y]) => {
+    let next = lastY + y;
+    donationDataCumulative.x.push(x);
+    donationDataCumulative.y.push(next);
+    lastY = next;
+  }, R.zip(donationDataTotal.x, donationDataTotal.y));
+
+
+  const donationDataCumulativeState = [];
+  const donationDataByJuri = [];
+  const prepJuriData = (dataObj, dataList) => R.mapObjIndexed((v, k, obj) => dataList.push({...v, name: k}), dataObj);
+  prepJuriData(donationDataCumulativeStateObj, donationDataCumulativeState);
+  prepJuriData(donationDataByJuriObj, donationDataByJuri);
+
+
+  console.log('DATA -> Donations sigma by state', donationDataCumulativeState);
+  console.log('DATA -> Donations Cumulative:', donationDataCumulative);
+  console.log('DATA -> Donations By Juri:', donationDataByJuri);
+  console.log('DATA -> Donations Total:', donationDataTotal);
+
+
+  Plotly.newPlot('donationGraphCumulativeDiv', [donationDataCumulative], {
+    title: "Cumulative Donations",
+    yaxis: {'rangemode': 'tozero', title: '$ AUD'},
+    xaxis: {title: 'Date'}
+  });
+
+  Plotly.newPlot('donationGraphCumulativeStateDiv', donationDataCumulativeState, {
+    title: "Cumulative Donations by Branch",
+    yaxis: {'rangemode': 'tozero', title: '$ AUD'},
+    xaxis: {title: 'Date'}
+  });
+
+  Plotly.newPlot('donationGraphTotalDiv', [donationDataTotal], {
+    title: "Donations this FY Total",
+    yaxis: {'rangemode': 'tozero', 'title': '$ AUD'},
+    xaxis: {title: 'Date'}
+  })
+
+  Plotly.newPlot('donationGraphByJuriDiv', donationDataByJuri, {
+    title: "Donations this FY by Branch",
+    yaxis: {'rangemode': 'tozero', 'title': '$ AUD'},
+    xaxis: {title: 'Date'}
+  })
+
+  return donationLog;
+}
