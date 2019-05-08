02db53d5f686bd895b12b6f574b09d6992aeeb4b
Max Kaye <m@xk.io>
2019-05-08 20:07:16 +1000

    add some dashboard stuff, comment stripe

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,2 +1,4 @@
 docker build -f ./_docker-dev/Dockerfile -t flux-website-docker-dev:latest .
-docker run -ti -p 9000:9000 --net=host -v "$PWD":/target flux-website-docker-dev
+
+export NODE_ENV="development"
+docker run -ti -p 9000:9000 --net=host -v "$PWD":/target --env NODE_ENV=$NODE_ENV flux-website-docker-dev
