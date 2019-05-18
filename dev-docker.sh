e03770c410d5f03ea56010f1b57a3d8ffb0c0dde
Max Kaye <m@xk.io>
2019-05-18 13:06:42 +1000

    add google optimize

--- dev-docker.sh
+++ dev-docker.sh
@@ -3,6 +3,6 @@ docker build -f ./_docker-dev/Dockerfile -t flux-website-docker-dev:latest .
 export NODE_ENV="development"
 _PWD=$(wslpath -w $PWD || echo $PWD)
 echo "mounting: $_PWD"
-docker run -ti -p 9000:9000 -v "$_PWD":/target --env NODE_ENV=$NODE_ENV flux-website-docker-dev
+docker run --network host -ti -p 9000:9000 -v "$_PWD":/target --env NODE_ENV=$NODE_ENV flux-website-docker-dev
 
 # --net=host
