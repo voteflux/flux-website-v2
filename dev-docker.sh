b421b941ff39fde44d83fc68911563f12455a0f4
Max Kaye <m@xk.io>
2019-09-30 08:38:19 +1000

    fix build issues on election calendar page + update docker method

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,8 +1,14 @@
 docker build -f ./_docker-dev/Dockerfile -t flux-website-docker-dev:latest .
 
 export NODE_ENV="development"
-_PWD=$(wslpath -w $PWD || echo $PWD)
-echo "mounting: $_PWD"
-docker run --network host -ti -p 9000:9000 -v "$_PWD":/target --env NODE_ENV=$NODE_ENV flux-website-docker-dev
+_PWD=$(wslpath -w $PWD 2>/dev/null || echo $PWD)
+_MNT="$_PWD:/target"
+echo "mounting: $_MNT"
+docker run -ti -p 9000:9000 \
+  --mount "type=bind,src=$_PWD,dst=/src" \
+  -w /src \
+  --env NODE_ENV=$NODE_ENV flux-website-docker-dev
 
+# --network host \
+# -v "$_MNT" -w /target \
 # --net=host
