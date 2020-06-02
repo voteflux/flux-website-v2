8d09c5a579be1ca4ef8937cc811f93befc4376e8
Max Kaye <m@xk.io>
2020-06-03 08:28:08 +1000

    update dev scripts

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,13 +1,23 @@
+#!/usr/bin/env bash
+
+# npm command to run
+ACTION="$1"
+
+set -x
+
 docker build -f ./_docker-dev/Dockerfile -t flux-website-docker-dev:latest .
 
 export NODE_ENV="development"
-_PWD=$(wslpath -w $PWD 2>/dev/null || echo $PWD)
+# don't need this with wsl2
+#_PWD=$(wslpath -w $PWD 2>/dev/null || echo $PWD)
+_PWD=$PWD
 _MNT="$_PWD:/target"
 echo "mounting: $_MNT"
-docker run -ti -p 9000:9000 \
+docker run -ti -p 9000:9000 --rm \
   --mount "type=bind,src=$_PWD,dst=/src" \
   -w /src \
-  --env NODE_ENV=$NODE_ENV flux-website-docker-dev
+  --env NODE_ENV=$NODE_ENV \
+  flux-website-docker-dev:latest npm run docker-dev -- ${ACTION:-flux}
 
 # --network host \
 # -v "$_MNT" -w /target \
