b7909d4071569db53f8c02dcadde8782e02746dd
Max Kaye <m@xk.io>
2020-06-04 18:36:27 +1000

    update docker setup to cache dependencies better

--- dev-docker.sh
+++ dev-docker.sh
@@ -4,6 +4,7 @@
 ACTION="$1"
 
 set -x
+set -e
 
 docker build -f ./_docker-dev/Dockerfile -t flux-website-docker-dev:latest .
 
@@ -29,13 +30,23 @@ esac
 #_PWD=$(wslpath -w $PWD 2>/dev/null || echo $PWD)
 
 _PWD=$PWD
-_MNT="$_PWD:/target"
-echo "mounting: $_MNT"
+
 docker run --rm $PORTS $EXTRA_ARGS \
+  --mount "type=volume,src=flux-site-vol-node,dst=/src/node_modules" \
+  --mount "type=volume,src=flux-site-vol-bundle-gems,dst=/src/.bundle-gems" \
+  --mount "type=volume,src=flux-site-vol-bundle,dst=/src/.bundle" \
   --mount "type=bind,src=$_PWD,dst=/src" \
   -w /src \
   --env NODE_ENV=$NODE_ENV \
-  flux-website-docker-dev:latest npm run --silent docker-dev -- ${ACTION:-flux}
+  flux-website-docker-dev:latest bash -c "npm ci && npm install webpack-cli && npm run ${ACTION:-flux}"
+  #flux-website-docker-dev:latest npm run --silent install-then -- ${ACTION:-flux}
+
+exit 0
+
+
+
+#_MNT="$_PWD:/target"
+#echo "mounting: $_MNT"
 
 # --network host \
 # -v "$_MNT" -w /target \
